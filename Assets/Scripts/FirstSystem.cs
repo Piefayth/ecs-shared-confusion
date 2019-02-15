fc157b98d514aa2f9a5f5901be45d0092b79dd55
Sean Ryan <piefayth@gmail.com>
2019-02-15 01:02:14 -0600

    job dependencies work

---
+++ Assets/Scripts/FirstSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using System.Collections.Generic;
+
+[UpdateBefore(typeof(SecondSystem))]
+public class FirstSystem : JobComponentSystem {
+    EntityManager entityManager;
+    List<SomeSharedType> allSharedTypes;
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        return inputDeps;
+    }
+
+    public NativeArrayToucher buildJobs() {
+        allSharedTypes.Clear();
+        entityManager.GetAllUniqueSharedComponentData(allSharedTypes);
+
+        return new NativeArrayToucher { dataHolder = allSharedTypes[1] };
+    }
+
+    protected override void OnCreateManager() {
+        entityManager = World.GetOrCreateManager<EntityManager>();
+        allSharedTypes = new List<SomeSharedType>();
+    }
+
+    protected override void OnDestroyManager() {
+    }
+}
+
+public struct NativeArrayToucher : IJob {
+    public SomeSharedType dataHolder;
+
+    public void Execute() {
+        dataHolder.data[0] = 5;
+    }
+}
---
+++ Assets/Scripts/SecondSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using System.Collections.Generic;
+
+[UpdateAfter(typeof(FirstSystem))]
+public class SecondSystem : JobComponentSystem {
+    EntityManager entityManager;
+    List<SomeSharedType> allSharedTypes;
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        return inputDeps;
+    }
+
+    public NativeArrayToucher buildJobs() {
+        allSharedTypes.Clear();
+        entityManager.GetAllUniqueSharedComponentData(allSharedTypes);
+
+        return new NativeArrayToucher { dataHolder = allSharedTypes[1] };
+    }
+
+    protected override void OnCreateManager() {
+        entityManager = World.GetOrCreateManager<EntityManager>();
+        allSharedTypes = new List<SomeSharedType>();
+    }
+
+    protected override void OnDestroyManager() {
+    }
+}
